5433b33a13cde1d45199e563392897cc537b4916
Aida Monfort Muriach <aida.monfort@uji.es>
2019-05-08 16:06:52 +0200

    add filtering for competences

--- src/app/views/newop/newop.component.ts
+++ src/app/views/newop/newop.component.ts
@@ -9,7 +9,8 @@ import * as bok from '@eo4geo/bok-dataviz';
 })
 export class NewopComponent implements OnInit {
 
-  competences = ['Team bulding', 'Think creatively', 'Manage time',
+  // TODO: Retrieve this from the DB
+  competences = ['Team building', 'Think creatively', 'Manage time',
     'Demonstrate intercultural competence', 'Work in an international environment', 'Demonstrate willingness to learn',
     'Develop strategy to solve problems', 'Identify opportunities', 'Work independently', 'Work efficientlyAdapt to change',
     'Digital competencies', 'Meet commitmentsAttend to detail', 'Interact with othersCope with pressure',
@@ -17,10 +18,22 @@ export class NewopComponent implements OnInit {
     'Customer relationship management', 'Show enterpreneurial spirit', 'Develop company strategies', 'Think proactively'
   ];
 
+  filteredCompetences = ['Team building', 'Think creatively', 'Manage time',
+    'Demonstrate intercultural competence', 'Work in an international environment', 'Demonstrate willingness to learn',
+    'Develop strategy to solve problems', 'Identify opportunities', 'Work independently', 'Work efficientlyAdapt to change',
+    'Digital competencies', 'Meet commitmentsAttend to detail', 'Interact with othersCope with pressure',
+    'Manage frustrationListen actively', 'Lead others', 'Assertiveness', 'Make decisions', 'Motivate others', 'Report facts',
+    'Customer relationship management', 'Show enterpreneurial spirit', 'Develop company strategies', 'Think proactively'
+  ];
+
+
   // tslint:disable-next-line:max-line-length
   model = new OcupationalProfile('id', 'Occupational Profile A', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vulputate non augue ac ornare. Duis pretium dictum elit vitae bibendum. Donec tristique tincidunt malesuada. Morbi a nulla urna. Praesent sit amet lectus ut nisi sodales pretium eu quis felis. Duis et felis ac risus aliquam iaculis eget nec metus. Vivamus porttitor auctor dolor et aliquam. Sed molestie lacus tellus, semper cursus ante mollis vel. Etiam vel massa mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec euismod dui. Quisque eget mattis turpis.', 'Maritime', 6, [], [], []);
 
-  constructor() { }
+  constructor() {
+
+    console.log('newOP');
+  }
 
   ngOnInit() {
     bok.visualizeBOKData('#bubbles', 'assets/saved-bok.xml');
@@ -28,13 +41,25 @@ export class NewopComponent implements OnInit {
 
   addCompetence(c: string) {
     this.model.competences.push(c);
-    this.removeCompetence(c, this.competences);
+    this.removeCompetence(c, this.filteredCompetences);
   }
 
   removeCompetence(name: string, array: string[]) {
-    array.forEach( (item, index) => {
+    array.forEach((item, index) => {
       if (item === name) { array.splice(index, 1); }
     });
+  }
+
+  filterCompetence(ev) {
+    let txt = event.target.value.toUpperCase();
+
+    this.filteredCompetences = [];
+    this.competences.forEach((item) => {
+      if (item.toUpperCase().indexOf(txt) !== -1) { this.filteredCompetences.push(item); }
+    });
+
+
+    console.log('filteringgggg');
   }
 
 }
