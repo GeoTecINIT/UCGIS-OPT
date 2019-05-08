4e25a4d0700c78e8dce970d540d76f9becebf958
Aida Monfort Muriach <aida.monfort@uji.es>
2019-05-08 10:07:14 +0200

    add bok visualizer and competences to new OP form

--- src/app/views/newop/newop.component.ts
+++ src/app/views/newop/newop.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OcupationalProfile } from '../../ocupational-profile';
+import * as bok from '@eo4geo/bok-dataviz';
 
 @Component({
   selector: 'app-newop',
@@ -17,11 +18,23 @@ export class NewopComponent implements OnInit {
   ];
 
   // tslint:disable-next-line:max-line-length
-  model = new OcupationalProfile('id', 'Occupational Profile A', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vulputate non augue ac ornare. Duis pretium dictum elit vitae bibendum. Donec tristique tincidunt malesuada. Morbi a nulla urna. Praesent sit amet lectus ut nisi sodales pretium eu quis felis. Duis et felis ac risus aliquam iaculis eget nec metus. Vivamus porttitor auctor dolor et aliquam. Sed molestie lacus tellus, semper cursus ante mollis vel. Etiam vel massa mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec euismod dui. Quisque eget mattis turpis.', 'Maritime', 6);
+  model = new OcupationalProfile('id', 'Occupational Profile A', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vulputate non augue ac ornare. Duis pretium dictum elit vitae bibendum. Donec tristique tincidunt malesuada. Morbi a nulla urna. Praesent sit amet lectus ut nisi sodales pretium eu quis felis. Duis et felis ac risus aliquam iaculis eget nec metus. Vivamus porttitor auctor dolor et aliquam. Sed molestie lacus tellus, semper cursus ante mollis vel. Etiam vel massa mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec euismod dui. Quisque eget mattis turpis.', 'Maritime', 6, [], [], []);
 
   constructor() { }
 
   ngOnInit() {
+    bok.visualizeBOKData('#bubbles', 'assets/saved-bok.xml');
+  }
+
+  addCompetence(c: string) {
+    this.model.competences.push(c);
+    this.removeCompetence(c, this.competences);
+  }
+
+  removeCompetence(name: string, array: string[]) {
+    array.forEach( (item, index) => {
+      if (item === name) { array.splice(index, 1); }
+    });
   }
 
 }
