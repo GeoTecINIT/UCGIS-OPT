cdc0b8809ec606832adbaf73ec33a25bb172f5e1
Aida Monfort Muriach <aida.monfort@uji.es>
2019-05-17 11:58:05 +0200

    fields service and fields raw data to add it to db

---
+++ src/app/services/fields.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import * as fData from './fields.data';
+
+const collection = 'Fields';
+
+export interface Field { name: string; code: Number; parent: string; grandparent: string; }
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FieldsService {
+
+  private db: AngularFirestore;
+  constructor(db: AngularFirestore) {
+    this.db = db;
+  }
+
+  addFields() {
+    fData.allFields.forEach(f => {
+      fData.parents.forEach(p => {
+        if (f.code.substring(0, 2) === p.code) {
+          f.greatgrandparent = p.name;
+        } else if (f.code.substring(0, 3) === p.code) {
+          f.grandparent = p.name;
+        } else if (f.code === p.code) {
+          f.parent = p.name;
+        }
+      });
+      this.db.collection(collection).add(f);
+    });
+  }
+}
+
