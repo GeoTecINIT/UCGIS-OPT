881db4684f0f6ef0afb1c278652f95373699c084
Agueda Gomez <gmz.cambronero@gmail.com>
2019-05-20 17:25:32 +0200

    Add search function in title and description. Add fields of advanced search

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -1,25 +1,45 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { NgForOf } from '@angular/common';
 import { Observable, Subscription } from 'rxjs';
 
 import { OcupationalProfile } from '../../ocupational-profile';
 import { OcuprofilesService } from '../../services/ocuprofiles.service';
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-  occupationalProfiles: Observable<OcupationalProfile[]>;
+  occupationalProfiles: OcupationalProfile[];
+  advancedSearch = false;
+  filteredOccuProfiles: any[];
+  searchText: string;
+  searchInKnowledges = false;
 
   constructor(private occuprofilesService: OcuprofilesService) {}
 
   ngOnInit() {
-    this.occupationalProfiles = this.occuprofilesService.subscribeToOccupationalProfiles();
+    this.occuprofilesService
+      .subscribeToOccupationalProfiles()
+      .subscribe(occuProfiles => {
+        this.occupationalProfiles = occuProfiles;
+        this.filteredOccuProfiles = occuProfiles;
+      });
   }
 
   removeOccuProfile(id: string) {
     this.occuprofilesService.removeOccuProfile(id);
+  }
+
+  filter() {
+    const search = this.searchText.toLowerCase();
+    this.filteredOccuProfiles = [];
+    this.filteredOccuProfiles = this.occupationalProfiles.filter(
+      it =>
+        it.title.toLowerCase().includes(search) ||
+        it.description.toLowerCase().includes(search)
+    );
   }
 }
