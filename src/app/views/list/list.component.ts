3504ea14fcbbcfb3e40bf8d6565a9fcb4bc52c2d
Aida Monfort Muriach <aida.monfort@uji.es>
2023-07-07 13:45:09 +0200

    change release notes and logo img

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -70,7 +70,11 @@ export class ListComponent implements OnInit {
       if (user) {
         this.isAnonymous = user.isAnonymous;
         this.userService.getUserById(user.uid).subscribe(userDB => {
-          this.currentUser = new User(userDB);
+          if (userDB) {
+            this.currentUser = new User(userDB);
+          } else {
+            this.userService.addNewUser(user);
+          }
           this.occuprofilesService
             .subscribeToOccupationalProfiles()
             .subscribe(occuProfiles => {
